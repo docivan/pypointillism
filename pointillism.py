cfa74de27a70869362fda82651ef1a087be04ecf
Dr. Ivan <i.o.potapenko@gmail.com>
2018-07-15 17:55:42 +0200

    v1

--- pointillism.py
+++ pointillism.py
@@ -2,51 +2,105 @@ import random
 import math
 import cairo
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
-scale = 0.1
-radius_max = 10
+radius_max = 50
 radius_min = 4
-segment_cnt = 8
-pos_jitter = 5
-rad_scale = 1.5
+alpha_min = 0.1
+points = 10000
 
 src_img = cv2.imread("eye.jpg")
-img = cv2.cvtColor(cv2.resize(src_img, (int(src_img.shape[1]*scale), int(src_img.shape[0]*scale))), cv2.COLOR_BGR2RGB)
+img_w = src_img.shape[1]
+img_h = src_img.shape[0]
 
-img_w = img.shape[1]
-img_h = img.shape[0]
+img = cv2.cvtColor(src_img, cv2.COLOR_BGR2RGB)
 
-#surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, img_w*radius_max*2, img_h*radius_max*2)
-surface = cairo.PDFSurface("example.pdf", img_w*radius_max*2, img_h*radius_max*2)
+lum = [0.2126 * x[0] + 0.7152 * x[1] + 0.0722 * x[2] for y in img for x in y]
+
+print(len(lum))
+print("mean=", np.mean(lum))
+print("median=", np.median(lum))
+print("std=", np.std(lum))
+
+bins = np.arange(0, 255, 10)
+plt.hist(lum, bins=bins)
+plt.title("Histogram")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+#plt.show()
+
+lum -= np.median(lum)
+lum /= np.std(lum)
+
+bins = np.arange(min(lum), max(lum), (max(lum)-min(lum))/20)
+plt.hist(lum, bins=bins)
+plt.title("Histogram")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+#plt.show()
+
+lum = np.abs(lum)
+
+bins = np.arange(min(lum), max(lum), (max(lum)-min(lum))/20)
+plt.hist(lum, bins=bins)
+plt.title("Histogram")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+#plt.show()
+
+print(min(lum), max(lum))
+
+lum_max = max(lum)
+
+lum_accum = []
+sum = 0
+
+for i in lum:
+    sum += i
+    lum_accum.append(sum)
+
+lum_sum = lum_accum[len(lum_accum)-1]
+
+
+def gen_random_xy():
+    a = lum_sum*random.random()
+    idx = -1
+
+    for i in range(len(lum_accum)):
+        if lum_accum[i] > a:
+            idx = i - 1
+            break
+
+    if idx < 0:
+        idx = 0
+
+    y = idx // img_w
+    x = idx - y*img_w
+    lumval = lum[idx]
+
+    return x, y, lumval
+
+
+#prep for exporting pdf
+surface = cairo.PDFSurface("example2.pdf", img_w, img_h)
 ctx = cairo.Context(surface)
 
-ctx.rectangle(0, 0, img_w*radius_max*2, img_h*radius_max*2)
+ctx.rectangle(0, 0, img_w, img_h)
 ctx.set_source_rgba(0,0,0,1)
 ctx.fill()
 
-for y in range(img_h):
-    for x in range(img_w):
-        r, g, b = img[y][x][0], img[y][x][1], img[y][x][2]
+for i in range(points):
+    if i % 100 == 0:
+        print(i)
 
-        # https://stackoverflow.com/questions/596216/formula-to-determine-brightness-of-rgb-color
-        lum = 0.2126 * r + 0.7152 * g + 0.0722 * b
-        local_rad_max = radius_min + (radius_max - radius_min) / 255 * lum
+    x, y, lumval = gen_random_xy()
 
-        center_x = x*radius_max*2 + radius_max + pos_jitter * random.random() - pos_jitter/2
-        center_y = y*radius_max*2 + radius_max + pos_jitter * random.random() - pos_jitter/2
+    r = radius_min + (radius_max-radius_min)*(1 - lumval/lum_max)
+    a = alpha_min + (1 - alpha_min)*(lumval/lum_max)
 
-        for i in range(segment_cnt):
-            radius = radius_min + (local_rad_max - radius_min) * random.random()
-            radius *= rad_scale
-            radians = 2 * math.pi / segment_cnt * i
-
-            vertex_x = math.cos(radians) * radius + center_x
-            vertex_y = math.sin(radians) * radius + center_y
-
-            ctx.line_to(vertex_x, vertex_y)
-
-        ctx.close_path()
-        ctx.set_source_rgba(r/255., g/255., b/255., 1)
-        ctx.fill()
+    ctx.arc(x, y, r, 0, 2*math.pi)
+    ctx.set_source_rgba(img[y][x][0]/255., img[y][x][1]/255., img[y][x][2]/255., a)
+    ctx.fill()
 
 surface.show_page()
